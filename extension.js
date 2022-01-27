98002846da819e74d86a7e59caf1f991b9166b20
eexpress <eexpress@163.com>
2022-01-28 02:37:12 +0800

    3

--- extension.js
+++ extension.js
@@ -1,110 +1,77 @@
 const { Clutter, Meta, Gdk, Shell } = imports.gi;
 const Main = imports.ui.main;
-//~ mixed fork from: panelScroll, JustP
-const POSITION = {
-    LEFT: 0,
-    RIGHT: 1
-};
+//~ mixed fork from: panelScroll, Just.P
 
 function lg(s){log("===Alt-Mouse===>"+s)}
 
 class PanelScroll {
-    constructor() {
-		this.wm = global.workspace_manager;
+	constructor() {
 		this.previousDirection = Meta.MotionDirection.UP;
 		this.listPointer = 0;
 		//~ this.scrollEventId = Main.panel.connect('scroll-event', this.scrollEvent.bind(this));
 		this.scrollEventId = global.stage.connect('scroll-event', this.scrollEvent.bind(this));
-    }
-//~ How can I get the `scroll-event` on any widget or say whole desktop, just like `Main.panel.connect('scroll-event',xxx)` does?
-//~ The ::scroll-event signal is emitted each time the mouse is scrolled on @actor
-//~ modifier_state 	Clutter.ModifierType 	r/w 	button modifiers
-//~ if event.modifier_state() == gdk::ModifierType::BUTTON2_MASK
-//~ https://valadoc.org/gdk-3.0/Gdk.ModifierType.html#!
+	}
+//event.modifier_state()
 //~ BUTTON1_MASK CONTROL_MASK SHIFT_MASK META_MASK SUPER_MASK MOD1_MASK( normally it is the Alt key)
-    scrollEvent(actor, event) {
-		//~ lg(event.get_state().toString(2));
-		//~ 11000 alt 10100 ctrl 1010000 super 10001 shift 10000 none
-		//~ if(event.get_state().toString(2) != '11000') return Clutter.EVENT_STOP;
-        let direction;
-        switch (event.get_scroll_direction()) {
-        case Clutter.ScrollDirection.UP:
-        case Clutter.ScrollDirection.LEFT:
-            direction = Meta.MotionDirection.UP;
-            break;
-        case Clutter.ScrollDirection.DOWN:
-        case Clutter.ScrollDirection.RIGHT:
-            direction = Meta.MotionDirection.DOWN;
-            break;
-        default:
-            return Clutter.EVENT_STOP;
-        }
+//~ 11000 alt 10100 ctrl 1010000 super 10001 shift 10000 none
+		scrollEvent(actor, event) {
+			//~ if(event.get_state().toString(2) != '11000') return Clutter.EVENT_STOP;
+			let direction;
+			switch (event.get_scroll_direction()) {
+			case Clutter.ScrollDirection.UP:
+			case Clutter.ScrollDirection.LEFT:
+				direction = Meta.MotionDirection.UP;
+				break;
+			case Clutter.ScrollDirection.DOWN:
+			case Clutter.ScrollDirection.RIGHT:
+				direction = Meta.MotionDirection.DOWN;
+				break;
+			default:
+				return Clutter.EVENT_STOP;
+		}
 
-        let gap = event.get_time() - this._time;
-        if (gap < 500 && gap >= 0)
-            return Clutter.EVENT_STOP;
-        this._time = event.get_time();
+		let gap = event.get_time() - this._time;
+		if (gap < 500 && gap >= 0)
+			return Clutter.EVENT_STOP;
+		this._time = event.get_time();
 
-        switch (this.pointerOnPanel()) {
-        case POSITION.LEFT:
-        case POSITION.RIGHT:
-                this.switchWindows(direction);
-            break;
-        }
+		this.switchWindows(direction);
 
-        return Clutter.EVENT_STOP;
-    }
+		return Clutter.EVENT_STOP;
+	}
 
-    pointerOnPanel() {
-        let [x, y, mod] =global.get_pointer();
+	switchWindows(direction) {
+		let windows = this.getWindows();
+		if (windows.length <= 1)
+			return;
 
-        let currentMonitor;
-        let currentMonitorIndex = global.display.get_current_monitor();
-        currentMonitor = global.display.get_monitor_geometry(currentMonitorIndex);
+		if (direction != this.previousDirection) {
+			this.listPointer = 1;
+		} else {
+			this.listPointer += 1;
+			if (this.listPointer > windows.length - 1)
+				this.listPointer = windows.length -1;
+		}
+		this.previousDirection = direction;
+		windows[this.listPointer].activate(global.get_current_time());
+	}
 
-        if (x < (currentMonitor.x + currentMonitor.width / 2))
-            return POSITION.LEFT;
+	getWindows() {
+		let cws = global.workspace_manager.get_active_workspace();
 
-        let aggregateMenu = Main.panel._rightBox.get_last_child().get_first_child();
-        if (x < (currentMonitor.x + currentMonitor.width - aggregateMenu.width))
-            return POSITION.RIGHT;
+		let windows = global.display.get_tab_list(Meta.TabList.NORMAL_ALL, cws);
+		windows.map(w => {
+			return w.is_attached_dialog() ? w.get_transient_for() : w;
+		}).filter((w, i, a) => !w.skip_taskbar && a.indexOf(w) == i);
 
-        return null;
-    }
+		return windows;
+	}
 
-    switchWindows(direction) {
-        let windows = this.getWindows();
-        if (windows.length <= 1)
-            return;
-
-        if (direction != this.previousDirection) {
-            this.listPointer = 1;
-        } else {
-            this.listPointer += 1;
-            if (this.listPointer > windows.length - 1)
-                this.listPointer = windows.length -1;
-        }
-        this.previousDirection = direction;
-        windows[this.listPointer].activate(global.get_current_time());
-    }
-
-    getWindows() {
-        let currentWorkspace = this.wm.get_active_workspace();
-
-        let windows = global.display.get_tab_list(Meta.TabList.NORMAL_ALL, currentWorkspace);
-        windows.map(w => {
-            return w.is_attached_dialog() ? w.get_transient_for() : w;
-        }).filter((w, i, a) => !w.skip_taskbar && a.indexOf(w) == i);
-
-		//~ lg(windows.length);
-        return windows;
-    }
-
-    destroy() {
-        if (this.scrollEventId != null) {
-            Main.panel.disconnect(this.scrollEventId);
-            this.scrollEventId = null;
-        }
+	destroy() {
+		if (this.scrollEventId != null) {
+			Main.panel.disconnect(this.scrollEventId);
+			this.scrollEventId = null;
+		}
    }
 }
 
@@ -115,11 +82,11 @@ function init(metadata) {
 
 function enable() {
 	lg("start");
-    panelScroll = new PanelScroll();
+	panelScroll = new PanelScroll();
 }
 
 function disable() {
 	lg("stop");
-    panelScroll.destroy();
-    panelScroll = null;
+	panelScroll.destroy();
+	panelScroll = null;
 }
