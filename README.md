6486f8ca36295dc9564e16497086047880c27d1a
eexpress <eexpress@163.com>
2022-01-29 14:23:05 +0800

    增加alt操作

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # gnome-shell-alt-mouse
 Alt + Mouse control window
 
-zone | scroll | butt 1|butt 2|butt 3
----|---|---|---|---
+- | scroll up/down | butt 1|butt 2(middle)|butt 3
+---|---|---|---|---|--
 panel | switch window|move[^3]|resize|lower[^2]
 desktop |switch window|move[^1][^3]|resize[^1]|lower[^1][^2]
+Alt + |fullscreen[^4]/|max[^3]|close|min[^2]
 
 [^1]: 鼠标三个按钮需要在桌面双击才有效。Need double-click
-[^2]: 窗口置底后，滚轮上滚，可立刻恢复原貌。
-[^3]: 有最大化窗口时，失效。似乎是窗口尺寸超过了面板一样，直接提升最大化的那个窗口。根本不进入鼠标按键事件。
+[^2]: 窗口置底/最小化后，滚轮下滚，可立刻恢复原貌。
+[^3]: 有最大化窗口时，窗口正上方的面板点击失效。似乎是窗口尺寸超过了面板一样，直接提升最大化的那个窗口。根本不进入鼠标按键事件。
+[^4]: 全屏后，窗口不能缩小，只能提前设置热键操作(比如Alt-F12)恢复。除开在全局取得控制权。
 
--- extension.js
+++ extension.js
@@ -3,51 +3,62 @@ const Main = imports.ui.main;
 const _backgroundMenu = imports.ui.backgroundMenu;
 //~ part fork from: panelScroll, Just Perfection
 
-function lg(s){log("===Alt-Mouse===>"+s)}
-const NeedAltKey = false;
+function lg(s){log("===Alt-Mouse===>"+s)};
 const DisableBGMenu = true;
+const maxflag = Meta.MaximizeFlags.VERTICAL
+//~ Meta.MaximizeFlags.BOTH
+//~ Meta.MaximizeFlags.HORIZONTAL
+//~ Meta.MaximizeFlags.VERTICAL
+
+//~ TODO:
+//~ 判断鼠标下面有没有窗口。window_under_pointer
+//~ ‌窗口去掉装饰条。不想使用外挂的xprop。
+//~ ‌窗口上，全局附加alt键。
+//~ ‌桌面的按键事件要去掉。目前只能双击，产生事件。
+//~ max后，面板正上方面板反正失效。
+//~ 全屏后，无法点击。需要全局控制权。
+//~ If a maximized window here, when use get_buffer_rect(), I got `-1,33, 1922x1048`, so large that made `button-release-event` fail? why the x is `-1` and the y is `33` not `32` ?
 
 class AltMouse {
 	constructor() {
 		this.previousDirection = Meta.MotionDirection.UP;
 		this.listPointer = 0;
-		if(DisableBGMenu){
-			this._originals = {};
-			this.backgroundMenuDisable();
-		}
+		this._originals = {};
+		if(DisableBGMenu) this.backgroundMenuDisable();
 
-		this.clickEventId = global.stage.connect('button-release-event', this.clickEvent.bind(this));
-		//~ 鼠标三个按钮需要在桌面双击才有效。
-//~ If a maximized window here, when use get_buffer_rect(), I got `-1,33, 1922x1048`, so large that made `button-release-event` fail? why the x is `-1` and the y is `33` not `32` ?
+		this.clickEventId = global.stage.connect('button-release-event', this.clickEvent.bind(this));	//~ 鼠标三个按钮需要在桌面双击才有效。
 		this.scrollEventId = global.stage.connect('scroll-event', this.scrollEvent.bind(this));
 	}
 
-//~ 判断鼠标下面有没有窗口。window_under_pointer
-//~ ‌桌面的按键事件要去掉。
-//~ ‌窗口去掉装饰条。不想使用外挂的xprop。
-//~ ‌窗口上，全局附加alt键。
 
 	clickEvent(actor, event){
-		if(NeedAltKey) if (!(event.get_state() & Clutter.ModifierType.MOD1_MASK)) return Clutter.EVENT_PROPAGATE;
+		const altkey = event.get_state() & Clutter.ModifierType.MOD1_MASK;
 
 		let w = global.display.get_focus_window();
 		switch (event.get_button()) {
 			case 1:
+				if(altkey){if(w.get_maximized() != maxflag){w.maximize(maxflag);}
+					else{w.unmaximize(maxflag);}
+					return Clutter.EVENT_STOP;
+				}
 				if(w.allows_move())
 				w.begin_grab_op(Meta.GrabOp.MOVING, true, event.get_time());
 				return Clutter.EVENT_STOP;
 				break;
 			case 2:	//middle click
+				if(altkey){if(w.can_close()) w.kill(); return Clutter.EVENT_STOP;}
 				if(w.allows_resize())
 				w.begin_grab_op(Meta.GrabOp.RESIZING_SE, true, event.get_time());	//Meta.GrabOp.RESIZING_UNKNOWN
 				return Clutter.EVENT_STOP;
 				break;
 			case 3:
+				if(altkey){if(w.can_minimize()) w.minimize(); return Clutter.EVENT_STOP;}
 				w.lower();
 				this.switchWindows(Meta.MotionDirection.UP);
 				return Clutter.EVENT_STOP;
 				break;
 			default:
+				return Clutter.EVENT_PROPAGATE;
 		}
 	};
 
@@ -69,15 +80,24 @@ class AltMouse {
 				direction = Meta.MotionDirection.DOWN;
 				break;
 			default:
-				return Clutter.EVENT_STOP;
+				return Clutter.EVENT_PROPAGATE;
 		}
 
-		let gap = event.get_time() - this._time;
-		if (gap < 500 && gap >= 0)
-			return Clutter.EVENT_STOP;
-		this._time = event.get_time();
-
-		this.switchWindows(direction);
+		//~ let gap = event.get_time() - this._time;
+		//~ if (gap < 500 && gap >= 0)
+			//~ return Clutter.EVENT_STOP;
+		//~ this._time = event.get_time();
+		if (event.get_state() & Clutter.ModifierType.MOD1_MASK){
+			let w = global.display.get_focus_window();
+			if(direction == Meta.MotionDirection.UP){
+				if(w.can_maximize()){
+					if(w.is_fullscreen()) w.unmake_fullscreen();
+					else w.make_fullscreen();
+				}
+			} else {
+				//~ if(w.can_minimize()) w.minimize();
+			}
+		} else this.switchWindows(direction);
 
 		return Clutter.EVENT_STOP;
 	}
@@ -109,15 +129,11 @@ class AltMouse {
 	}
 
 	backgroundMenuEnable() {
-		if (this._originals['backgroundMenu']) _backgroundMenu.BackgroundMenu.prototype.open
-		= this._originals['backgroundMenu'];
+		if (this._originals['bgMenu']) _backgroundMenu.BackgroundMenu.prototype.open = this._originals['bgMenu'];
 	}
 
 	backgroundMenuDisable() {
-		if (!this._originals['backgroundMenu']) {
-			this._originals['backgroundMenu']
-			= _backgroundMenu.BackgroundMenu.prototype.open;
-		}
+		if (!this._originals['bgMenu']) this._originals['bgMenu'] = _backgroundMenu.BackgroundMenu.prototype.open;
 
 		_backgroundMenu.BackgroundMenu.prototype.open = () => {};
 	}
